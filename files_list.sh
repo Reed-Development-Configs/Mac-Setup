501da82be353d8274f108c3af9d813734c7b6f6b
MrT <turgeon.dev@gmail.com>
2025-08-27 11:21:54 -0400

    removed missing file from files_list.sh

--- files_list.sh
+++ files_list.sh
@@ -4,7 +4,6 @@
 # This array contains the paths to the scripts that need to be executed during setup.
 # Modify this list if you add more setup scripts or change their locations.
 FILES=(
-    "./setup_dotfiles.sh"
     "./install_packages.sh"
     "./customize_mac.sh"
 )
